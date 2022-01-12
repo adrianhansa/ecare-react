3eb6b11ffc8710ed6c930b89476891c64eee3fd1
Adrian Hansa <adrian.hansa@yahoo.com>
2022-01-12 07:25:44 +0000

    Implemented react-router-dom and react-router-bootstrap; created AppNavbar component.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Login from "./pages/Login";
+import AppNavbar from "./components/AppNavbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div>
-        <h1>e-Care</h1>
-        <Login />
-      </div>
+      <BrowserRouter>
+        <AppNavbar />
+        <Routes>
+          <Route to="/" element={<Home />} />
+          <Route to="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import axios from "axios";
-axios.defaults.withCredentials = true;
+import "bootstrap/dist/css/bootstrap.min.css";
 
+axios.defaults.withCredentials = true;
 ReactDOM.render(<App />, document.getElementById("root"));
