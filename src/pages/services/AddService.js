47b83e5de13f4af67ffeef6c578f6147be4e2627
Adrian Hansa <adrian.hansa@yahoo.com>
2022-01-14 18:35:20 +0000

    Created AddService modal.

---
+++ src/pages/services/AddService.js
@@ -0,0 +1,99 @@
+import React from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import { Formik } from "formik";
+import * as yup from "yup";
+import { addService } from "../../redux/actions/serviceActions";
+import { useDispatch, useSelector } from "react-redux";
+
+const AddService = ({ show, handleClose }) => {
+  const dispatch = useDispatch();
+  const serviceDetails = useSelector((state) => state.serviceDetails);
+  const validationSchema = yup.object({
+    name: yup.string().required("Please provide a name for the service."),
+    address: yup
+      .string()
+      .required("Please provide an address for the service."),
+    phone: yup
+      .string()
+      .required("Please provide a phone number for the service."),
+  });
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Add a New Service</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        {serviceDetails.error && (
+          <p className="text-danger">{serviceDetails.error}</p>
+        )}
+        <Formik
+          initialValues={{ name: "", address: "", phone: "" }}
+          validationSchema={validationSchema}
+          onSubmit={(values) => {
+            dispatch(addService(values));
+            if (!serviceDetails.error) handleClose();
+          }}
+        >
+          {(props) => (
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Service name"
+                  value={props.values.name}
+                  onChange={props.handleChange("name")}
+                  onBlur={props.handleBlur("name")}
+                />
+                {props.touched.name && (
+                  <p className="text-danger">{props.errors.name}</p>
+                )}
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicAddress">
+                <Form.Label>Address</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Service address"
+                  value={props.values.address}
+                  onChange={props.handleChange("address")}
+                  onBlur={props.handleBlur("address")}
+                />
+                {props.touched.address && (
+                  <p className="text-danger">{props.errors.address}</p>
+                )}
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPhone">
+                <Form.Label>Phone</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Phone number"
+                  value={props.values.phone}
+                  onChange={props.handleChange("phone")}
+                  onBlur={props.handleBlur("phone")}
+                />
+                {props.touched.phone && (
+                  <p className="text-danger">{props.errors.phone}</p>
+                )}
+              </Form.Group>
+              <Button
+                variant="primary"
+                type="submit"
+                className="me-3"
+                onClick={props.handleSubmit}
+              >
+                Add Service
+              </Button>
+              <Button variant="secondary" onClick={handleClose}>
+                Cancel
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default AddService;
--- src/pages/services/Services.js
+++ src/pages/services/Services.js
@@ -1,13 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Container, Col, Row, Card, Button } from "react-bootstrap";
 import { getServices } from "../../redux/actions/serviceActions";
+import AddService from "./AddService";
+import { GrAddCircle } from "react-icons/gr";
 
 const Services = () => {
   const dispatch = useDispatch();
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+
   const { services, loading, error } = useSelector(
     (state) => state.serviceList
   );
+
   useEffect(() => {
     dispatch(getServices());
   }, [dispatch]);
@@ -16,18 +23,31 @@ const Services = () => {
       <Row className="mt-3">
         {error && <p className="text-danger">{error}</p>}
         {loading && <p>Loading...</p>}
-        {services &&
-          services.map((service) => (
-            <Col sm={3} md={4} lg={3} key={service._id}>
-              <Card className="mb-4">
-                <Card.Header>{service.name}</Card.Header>
-                <Card.Body>{service.address}</Card.Body>
-                <Card.Footer>
-                  <Button className="primary">Access</Button>
-                </Card.Footer>
-              </Card>
+        <Col>
+          <Row>
+            <Col className="mx-auto">
+              <h3 className="text-center">
+                Services{" "}
+                <GrAddCircle type="button" onClick={() => setShow(true)} />
+              </h3>
             </Col>
-          ))}
+          </Row>
+          <AddService show={show} handleClose={handleClose} />
+          <Row>
+            {services &&
+              services.map((service) => (
+                <Col sm={3} md={4} lg={3} key={service._id}>
+                  <Card className="mb-4">
+                    <Card.Header>{service.name}</Card.Header>
+                    <Card.Body>{service.address}</Card.Body>
+                    <Card.Footer>
+                      <Button className="primary">Access</Button>
+                    </Card.Footer>
+                  </Card>
+                </Col>
+              ))}
+          </Row>
+        </Col>
       </Row>
     </Container>
   );
