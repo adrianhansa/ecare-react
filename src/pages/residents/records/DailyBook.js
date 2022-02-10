46d92b34aca94d65e075de7a44e0312872747295
Adrian Hansa <adrian.hansa@yahoo.com>
2022-02-10 21:43:48 +0000

    add a different column in Daily Book componemet to display previous records; color differently the books for each shift

--- src/pages/residents/records/DailyBook.js
+++ src/pages/residents/records/DailyBook.js
@@ -13,7 +13,7 @@ const DailyBook = () => {
   const su = useSelector((state) => state.serviceUserDetails);
   const { slug, resident } = useParams();
 
-  const [date, setDate] = useState(moment(new Date()).format("YYYY-MM-DD"));
+  const date = moment(new Date()).format("YYYY-MM-DD");
   const shifts = ["AM", "PM", "Night"];
 
   useEffect(() => {
@@ -25,44 +25,59 @@ const DailyBook = () => {
   return (
     <Container fluid>
       <Row>
-        {su.loading && <p>Loading...</p>}
-        {su.error && <p className="text-danger">{su.error}</p>}
-        {su.serviceUser && (
-          <>
-            <h2 className="text-center">
-              Daily records for {su.serviceUser.name}
-            </h2>
-            {shifts.map((shift) => {
-              return (
-                <Col
-                  key={shift}
-                  xl={3}
-                  lg={3}
-                  md={3}
-                  sm={4}
-                  xs={4}
-                  className="mx-auto"
-                >
-                  <Link
-                    to={`/services/daily-records/${slug}/${resident}/${date}/${shift}`}
-                    style={{ textDecoration: "none" }}
+        <Col md="auto">
+          <h3>Previous Records</h3>
+        </Col>
+        <Col>
+          {su.loading && <p>Loading...</p>}
+          {su.error && <p className="text-danger">{su.error}</p>}
+          {su.serviceUser && (
+            <Row>
+              <h2 className="text-center">
+                Daily records for {su.serviceUser.name}
+              </h2>
+              {shifts.map((shift) => {
+                return (
+                  <Col
+                    key={shift}
+                    xl={3}
+                    lg={3}
+                    md={3}
+                    sm={4}
+                    xs={4}
+                    className="mx-auto"
                   >
-                    <Card>
-                      <Card.Header className="text-center">
-                        <img
-                          src="/images/Book.png"
-                          alt="Record Daily Book"
-                          height={120}
-                        />
-                      </Card.Header>
-                      <Card.Body className="text-center">{shift}</Card.Body>
-                    </Card>
-                  </Link>
-                </Col>
-              );
-            })}
-          </>
-        )}
+                    <Link
+                      to={`/services/daily-records/${slug}/${resident}/${date}/${shift}`}
+                      style={{ textDecoration: "none" }}
+                    >
+                      <Card>
+                        <Card.Header
+                          className={
+                            shift === "AM"
+                              ? "bg-info text-center"
+                              : shift === "PM"
+                              ? "bg-success text-center"
+                              : shift === "Night"
+                              ? "bg-warning text-center"
+                              : "text-center"
+                          }
+                        >
+                          <img
+                            src="/images/Book.png"
+                            alt="Record Daily Book"
+                            height={120}
+                          />
+                        </Card.Header>
+                        <Card.Body className="text-center">{shift}</Card.Body>
+                      </Card>
+                    </Link>
+                  </Col>
+                );
+              })}
+            </Row>
+          )}
+        </Col>
       </Row>
     </Container>
   );
