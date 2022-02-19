b163a970980604331c2590f816c08c8e868158e6
Adrian Hansa <adrian.hansa@yahoo.com>
2022-02-19 14:10:45 +0000

    Displaying a full year calendar in prep for highlighting the absences.

--- src/pages/absences/Absences.js
+++ src/pages/absences/Absences.js
@@ -5,14 +5,22 @@ import { getAbsencesByEmployee } from "../../redux/actions/absenceActions";
 import AbsenceRecording from "./AbsenceRecording";
 import { MdOutlineSick } from "react-icons/md";
 import moment from "moment";
+import enumerateDaysBetweenDates from "../../utils/enumerateDays";
+import { Row, Col, Container } from "react-bootstrap";
 
 const Absences = () => {
   const [startDate, setStartDate] = useState(
-    moment(new Date()).format("MM-DD-YYYY")
+    moment(new Date())
+      .startOf("week")
+      .add(1, "day")
+      .add(-364, "days")
+      .format("MM-DD-YYYY")
   );
   const [endDate, setEndDate] = useState(
     moment(new Date()).format("MM-DD-YYYY")
   );
+
+  const days = enumerateDaysBetweenDates(startDate, endDate);
 
   const { slug, employee } = useParams();
   const dispatch = useDispatch();
@@ -25,9 +33,24 @@ const Absences = () => {
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
+  const daysOfTheWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  const results = daysOfTheWeek.map((weekDay) => {
+    return days.filter((day) => {
+      return weekDay === moment(day).format("dddd");
+    });
+  });
 
   return (
-    <div>
+    <Container fluid>
       <h1>
         Absences{" "}
         <MdOutlineSick
@@ -43,7 +66,46 @@ const Absences = () => {
         employee={employee}
         service={slug}
       />
-    </div>
+      <Row>
+        {daysOfTheWeek.map((weekDay) => {
+          return (
+            <Col style={{ fontSize: 8 }} key={weekDay}>
+              {weekDay}
+            </Col>
+          );
+        })}
+      </Row>
+      <Row>
+        {results.map((result, index) => {
+          return (
+            <Col key={index}>
+              {result.map((r, i) => {
+                return (
+                  <Row key={i} style={{ marginBottom: 2 }}>
+                    <Col
+                      className="text-center"
+                      style={{
+                        fontSize: 8,
+                        border: "1px solid grey",
+                        padding: 10,
+                        margin: 2,
+                        borderRadius: 5,
+                        hover: {
+                          fontSize: 12,
+                          color: "red",
+                        },
+                      }}
+                    >
+                      {moment(r).format("DD-MM-YYYY")}
+                    </Col>
+                  </Row>
+                );
+              })}
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
   );
 };
 
